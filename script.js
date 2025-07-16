e434fdba042d8b2e08471a22fccfa1e9fe46c1c2
Sheila Khayeshe <khayeshesheila2@gmail.com>
2025-07-16 04:47:14 +0300

    Updated  project structure and fixed image paths.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbweaBg7uO0UWKuxkFIkdCp4VM8mdswaidXwBBI_iKWoJxPknKK0UrRS2qTO_R8mQ4GZAQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const message=document.querySelector("#msg")
+  
 
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        message.innerHTML='Message has been sent successfully'
+        setTimeout(function (){
+            message.innerHTML=""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
